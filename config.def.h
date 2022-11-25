b6a57633f4fc2d8771bee901b75d956c1e09d0c8
Matías Olcese <clear6860@tutanota.com>
2022-11-24 22:57:23 -0300

    Add glava to the unmanaged list

--- config.def.h
+++ config.def.h
@@ -46,8 +46,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor   ignoretransient unmanaged */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1,       0               0 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,       0               0 },
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1,       0               0 },
+	{ "Librewolf", NULL,       NULL,       1 << 8,       0,           -1,       0               0 },
+	{ "glava",     NULL,       NULL,       0,            0,           -1,       0,              1 },
 };
 
 /* layout(s) */
